ad6c41a1ab29a075b6742123e85c85b165ad966f
Sabrinaazh <sabrinanurazzahraa@gmail.com>
2022-10-03 19:45:40 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,35 @@
+alert("SELAMAT DATANG")
+function check()
+    {
+        let cb = document.getElementById('cb').checked;
+        if(cb == true)
+        {
+            document.getElementById('kepala').classList.remove('header');
+            document.getElementById('container').classList.remove('konten');
+            document.getElementById("kepala").classList.add("headerDark");
+            document.getElementById('container').classList.add('dark');
+                
+        }
+
+        else
+        {
+            document.getElementById('kepala').classList.remove("headerDark");
+            document.getElementById('container').classList.remove('dark');
+            document.getElementById("kepala").classList.add('header');
+            document.getElementById('container').classList.add('konten');
+        }
+    }
+
+    let button = document.getElementById('info');
+    button.addEventListener("click", function(){
+        let x = document.getElementById("more");
+        if(x.style.display == "none")
+        {
+            x.style.display = "block";
+        }
+
+        else
+        {
+            x.style.display = "none";
+        }
+    })
